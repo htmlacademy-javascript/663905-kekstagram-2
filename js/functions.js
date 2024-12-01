17bfb54be975bdfb057af464b7344ac4a7590f02
arsen <arsbestone@gmail.com>
2024-12-01 20:32:55 +0300

    Функции для тренировки (исправил ошибки и добавил третью функцию)

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,45 @@
-function checkStringLenght (str, maxlength) {
-  return str.lenght <= maxlength;
+function checkStringLength (str, maxLength) {
+  return str.length <= maxLength;
 }
-checkStringLenght('проверяемая строка', 20);
-checkStringLenght('проверяемая строка', 18);
-checkStringLenght('проверяемая строка', 10);
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
 
 function isPalindrome (str) {
   const normalizedStr = str.replaceAll(' ', '').toLowerCase();
   let reversedStr = '';
-  for (let i = normalizedStr.lenght - 1; i >= 0; i--) {
+  for (let i = normalizedStr.length - 1; i >= 0; i--) {
     reversedStr += normalizedStr[i];
   }
   return normalizedStr === reversedStr;
 }
-isPalindrome('топот');
-isPalindrome('ДовОд');
-isPalindrome('Кекс');
-isPalindrome('Лёша на полке клопа нашёл ');
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('ДовОд'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл '));
+
+let extractDigits = function(input) {
+  let str = input.toString();
+
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+
+      if (!Number.isNaN(parseInt(char))) {
+          result += char;
+      }
+  }
+
+  return result === '' ? NaN : parseInt(result);
+};
+
+console.log(extractDigits('2023 год'));
+console.log(extractDigits('ECMAScript 2022'));
+console.log(extractDigits('1 кефир, 0.5 батона'));
+console.log(extractDigits('агент 007'));
+console.log(extractDigits('а я томат'));
+
+console.log(extractDigits(2023));
+console.log(extractDigits(-1));
+console.log(extractDigits(1.5));
